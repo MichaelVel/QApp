fcdc7188023d6a4a707f3d4eb2efed44a07812d9
Michael Vel <msvelandiag@unal.edu.co>
2022-08-29 15:16:26 -0500

    setting up login system

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def index(request): 
+    return render(request,'quiz/index.html')
+
+def login_view(request): 
+    username = request.POST['username']
+    password = request.POST['password']
+    
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request,user)
+
+    return render(request,'quiz/index.html')
+    
+
