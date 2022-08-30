ae65fc89ed7e6d928caf528dc98e986bbea719cc
Michael Vel <msvelandiag@unal.edu.co>
2022-08-30 11:41:56 -0500

    add MainView navegation

--- quiz/views.py
+++ quiz/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 def index(request): 
     return render(request,'quiz/index.html')
 
 def login_view(request): 
-    username = request.POST['username']
-    password = request.POST['password']
-    
-    user = authenticate(username=username, password=password)
-
+    user = authenticate(username=request.POST['username'],
+                        password=request.POST['password'])
     if user is not None:
         login(request,user)
+    return redirect(index)
 
-    return render(request,'quiz/index.html')
-    
+def logout_view(request):
+    logout(request)
+    return redirect(index)
+
 
