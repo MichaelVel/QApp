093216165be0b6c80b6dc235435e92bf1f2c328e
Michael Vel <msvelandiag@unal.edu.co>
2022-08-30 12:03:12 -0500

    add models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    users = models.ManyToManyField(User, through='Answer')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField("choice is correct?")
+
+class Answer(models.Model):
+    session = models.BigIntegerField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
