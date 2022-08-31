b26940334483f98b81db0a6e0c678b514456f06e
Michael Vel <msvelandiag@unal.edu.co>
2022-08-31 14:26:09 -0500

    format register form

--- registerUsers/forms.py
+++ registerUsers/forms.py
@@ -1,20 +1,41 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import EmailInput, PasswordInput, TextInput
 
 
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+    password1 = forms.CharField(label="Password",
+            strip=False,
+            widget=forms.PasswordInput(attrs={
+                "class": "form-control",
+                "autocomplete": "new-password"}))
 
-	class Meta:
-		model = User
-		fields = ("username", "email", "password1", "password2")
+    password2 = forms.CharField(label="Password Confirmation",
+            strip=False,
+            widget=forms.PasswordInput(attrs={
+                "class": "form-control",
+                "autocomplete": "new-password"}))
 
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
-		if commit:
-			user.save()
-		return user
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+        widgets = { 
+                'username': TextInput(attrs={
+                    'class': "form-control",
+                    }),
+                'email': EmailInput(attrs={
+                    'class': "form-control",
+                    }),
+        }
+
+    
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
