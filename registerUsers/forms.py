5220f1cfb39cc2fe0ba58f785fb34c0648bfba4a
Michael Vel <msvelandiag@unal.edu.co>
2022-09-01 09:15:58 -0500

    modify the registerUsers view

--- registerUsers/forms.py
+++ registerUsers/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import EmailInput, PasswordInput, TextInput
+from django.forms import EmailInput, TextInput
 
 
 # Create your forms here.
--- registerUsers/views.py
+++ registerUsers/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
 from .forms import NewUserForm
 
@@ -7,9 +7,8 @@ def register(request):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+            user = form.save()
+            login(request,user)
             return redirect("/")
 
     # if a GET (or any other method) we'll create a blank form
