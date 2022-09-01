885f2272aa90cc14d8c2b402f693e168785aaea8
Michael Vel <msvelandiag@unal.edu.co>
2022-09-01 12:47:20 -0500

    create QuizViews

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('quiz/start', views.start, name='start'),
+        path('quiz/<int:question_id>/', views.question, name='question'),
+        path('quiz/finish', views.end, name='end'),
+        path('results', views.results, name='results'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,30 @@
+import random
+
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
 from django.contrib.auth import views as auth_views
+
+from .models import Question
 
 def index(request): 
     return render(request,'quiz/index.html')
 
+def start(request):
+    questions_ids = list(Question.objects.values('id'))
+    questions_ids = list(map(lambda x: x["id"], questions_ids))
+
+    questions_selected = random.sample(questions_ids,3)
+    first_question = questions_selected.pop()
+
+    request.session['questions'] = questions_selected
+    return redirect(question, question_id=first_question)
+
+def question(request, question_id):
+    return HttpResponse(question_id)
+
+def end(request):
+    pass
+
+def results(request):
+    pass
