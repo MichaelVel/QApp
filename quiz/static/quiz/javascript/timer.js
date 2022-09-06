3050f6446207ef868609c45f1c1267b80faa1052
Michael Vel <msvelandiag@unal.edu.co>
2022-09-06 12:35:32 -0500

    impl timer

---
+++ quiz/static/quiz/javascript/timer.js
@@ -0,0 +1,13 @@
+const time = window.setInterval(changeTimer,1000);
+
+window.setTimeout(function myFunction() {
+  clearInterval(time);
+  document.getElementById("submit-btn").click();
+},16000);
+
+function changeTimer() {
+  let currSec = document.getElementById("timer").value;
+  document.getElementById("timer").value = currSec-1;
+}
+
+
