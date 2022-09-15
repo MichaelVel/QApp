53236d82cd0bd8f16e8a9876a5780adfe1b62963
Michael Vel <msvelandiag@unal.edu.co>
2022-09-15 10:54:57 -0500

    tests and model typing

--- quiz/models.py
+++ quiz/models.py
@@ -1,32 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Sum
+from django.db.models import QuerySet, Sum
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     users = models.ManyToManyField(User, through='Answer')
+    objects = models.Manager()
     
     def __str__(self):
        return self.question_text 
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField("choice is correct?")
+    objects = models.Manager()
 
     def is_correct_answer(self):
         return self.is_correct
 
+    def __str__(self):
+        return self.choice_text
+
 
 class AnswerSessionManager(models.Manager):
-    def get_last_session(self, user_id):
-        ls = self.filter(user=user_id).aggregate(models.Max('session'))['session__max']
+    def get_last_session(self, user_id: int) -> int:
+        ls: int= self.filter(user=user_id).aggregate(models.Max('session'))['session__max']
         return 0 if not ls else ls
+    
+    def get_correct_choice(self, question: Question) -> Choice:
+        return Choice.objects.filter(question=question, is_correct=True)[0]
 
-    def score(self, user_id, session_id):
-        score = self.filter(user__id=user_id, session=session_id)
+
+    def score(self, user_id: int, session_id: int) -> int:
+        score: QuerySet = self.filter(user__id=user_id, session=session_id)
         score = score.exclude(choice__is_correct=False)
         score = score.aggregate(score=Sum('time'))
 
@@ -36,7 +44,7 @@ class AnswerSessionManager(models.Manager):
         return score['score']
 
     def top5_scores(self):
-        scores = self.exclude(choice__is_correct=False)
+        scores: QuerySet = self.exclude(choice__is_correct=False)
         scores = scores.values('user__username','session')
         scores = scores.annotate(score=Sum('time'))
         scores = scores.order_by('-score')[:5]
--- quiz/tests.py
+++ quiz/tests.py
@@ -3,33 +3,61 @@ from django.test import TestCase
 from django.utils import timezone
 from .models import Answer, Question, Choice
 
-# Create your tests here.
+class MockFactory:
+    @staticmethod
+    def empty_questions(n_q: int) -> list[Question]:
+        questions: list[Question] = []
+
+        for i in range(1, n_q+1):
+            questions.append(Question(question_text = f"q{i}",
+                pub_date = timezone.now()))
+            
+        return questions
+    
+    @staticmethod
+    def generic_choices(question: Question, n_ans: int) -> list[Choice]:
+        answers: list[Choice] = []
+
+        for _ in range(n_ans-1):
+            answers.append(Choice(choice_text="false",
+                question=question, is_correct=False))
+         
+        answers.append(Choice(choice_text="true", 
+            question=question, is_correct=True))
+
+        return answers
+
+    @staticmethod
+    def test_user(n:int) -> User:
+        return User(username=f"userTestCase{n}", password="pass")
+
+    @staticmethod
+    def test_answer(session:int, question:Question,
+            user: User, choice: Choice, time: int) -> Answer: 
+        return Answer(session=session,
+                question=question,
+                user=user,
+                choice= choice,
+                pub_date = timezone.now(),
+                time = time
+                )
+
 
 class TestAnswerModel(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.questions: dict[str,Question] = {}
-        for i in range(3):
-            # Creating questions
-            question = Question(question_text= f"question{i}",
-                    pub_date=timezone.now())
+        cls.questions = MockFactory.empty_questions(3)
+
+        for question in cls.questions:
             question.save()
-            cls.questions[f"question{i}"] = question
-
-            for i in range(2):
-                choice = Choice(choice_text= f"false",
-                        question=question, 
-                        is_correct=False)
+            question_choices = MockFactory.generic_choices(question, 3)
+            for choice in question_choices:
                 choice.save()
+        
+        cls.user1 = MockFactory.test_user(1)
+        cls.user2 = MockFactory.test_user(2)
 
-            choice_correct = Choice(choice_text="true",
-                    question=question,
-                    is_correct=True)
-            choice_correct.save()
-
-        cls.user1 = User(username='userTestCase1', password="pass")
         cls.user1.save()
-        cls.user2 = User(username='userTestCase2', password="pass")
         cls.user2.save()
 
     def test_get_last_session_when_user_has_not_played(self):
@@ -40,7 +68,7 @@ class TestAnswerModel(TestCase):
         n = 5
         for i in range(n+1):
             answer = Answer(session=i,
-                question= self.questions["question1"],
+                question= self.questions[1],
                 user= self.user1,
                 pub_date=timezone.now())
             answer.save()
@@ -50,7 +78,27 @@ class TestAnswerModel(TestCase):
         
 
     def test_get_score(self):
-        pass
+        for question in self.questions:
+            answer = MockFactory.test_answer(1,
+                    question,
+                    self.user2,
+                    Answer.sessions.get_correct_choice(question),
+                    10)
+            answer.save()
+
+        score = Answer.sessions.score(self.user2.id,1)
+        self.assertEqual(score,30)
 
     def test_get_top5_scores(self):
-        pass
+        for i in range(1,8):
+            for question in self.questions:
+                answer = MockFactory.test_answer(i,
+                        question,
+                        self.user2,
+                        Answer.sessions.get_correct_choice(question),
+                        i*5)
+                answer.save()
+        expected = [(7*5*3), (6*5*3), (5*5*3), (4*5*3), (3*5*3)]
+        scores = Answer.sessions.top5_scores()
+        for score, expected_score in zip(scores, expected):
+            self.assertEqual(score['score'],expected_score)
