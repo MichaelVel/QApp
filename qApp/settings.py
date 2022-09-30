05390ee8d007dbb587a2419425886bf890577a83
Michael Vel <msvelandiag@unal.edu.co>
2022-09-30 09:54:56 -0500

    removing credential module from config

--- qApp/settings.py
+++ qApp/settings.py
@@ -16,8 +16,6 @@ import os
 from django.test.runner import DiscoverRunner
 from pathlib import Path
 
-from qApp.credentials import DATABASE_PASSWORD
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
