3d4e44638b0f7d341d4f6ede7724eb15fbb72fe3
sebastian <msvelandiag@unal.edu.co>
2023-02-08 19:05:12 -0500

    create fastAPI project

--- quiz/tests/exceptions.py
+++
@@ -1,8 +0,0 @@
-
-class TestException(Exception):
-    """ base class for testing package exceptions """
-
-class  PlayRoundException(TestException):
-    """ 
-    Raised if something goes wrong in the generation of mock rounds 
-    """
